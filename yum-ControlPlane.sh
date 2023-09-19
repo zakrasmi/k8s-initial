76150bd487203f8904b55186619d7de79a897f63
zakrasmi <145234986+zakrasmi@users.noreply.github.com>
2023-09-19 19:18:24 +0000

    Update yum-ControlPlane.sh

--- yum-ControlPlane.sh
+++ yum-ControlPlane.sh
@@ -39,6 +39,7 @@ echo -e "+++"
 source <(kubectl completion bash) # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
 echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
 alias k=kubectl
+echo "alias k=kubectl" >> ~/.bashrc
 complete -o default -F __start_kubectl k
 
 echo
