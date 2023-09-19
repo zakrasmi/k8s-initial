13504436b0d140e4342eda93b437cfc5c63cbe98
zakrasmi <145234986+zakrasmi@users.noreply.github.com>
2023-09-19 20:22:07 +0000

    Update 2k8s-vim.sh

--- 2k8s-vim.sh
+++ 2k8s-vim.sh
@@ -41,8 +41,28 @@ sudo wget -q $LATEST -O /usr/bin/yq
 sudo chmod +x /usr/bin/yq
 
 cat <<EOT >> $HOME/.vimrc
-line 1
-line 2
+" ls -l /usr/share/vim/vim80/colors
+"""colorscheme torte
+set paste
+set ruler
+
+""" basic settings for yaml and python files
+"autocmd FileType python setlocal ts=4 sts=4 sw=4 expandtab number autoindent
+"autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab autoindent
+autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
+
+
+let g:indentLine_char = '⦙'
+let g:ale_echo_msg_format = '[%linter%] %s [%severity%]'
+let g:ale_sign_error = '✘'
+let g:ale_sign_warning = '⚠'
+let g:ale_lint_on_text_changed = 'never'
+
+""" folding can help troubleshoot indentation syntax
+set foldenable
+set foldlevelstart=20
+set foldmethod=indent
+nnoremap <space> za
 EOT
 
 echo
